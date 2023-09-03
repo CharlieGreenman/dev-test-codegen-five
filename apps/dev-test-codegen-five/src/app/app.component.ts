31a1bee084b090d0d9a2b5ddb05fce0b3f1841e4
Charlie Greenman <charlie@razroo.com>
2023-09-03 02:49:26 -0700

    Create starter files for angular-16.2.0

---
+++ apps/dev-test-codegen-five/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dev-test-codegen-five-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dev-test-codegen-five';
+}
